e51ee55c09082aa932396e7f70d4f7fc79aac8a2
GonzoGhanem <ghanemgo@gmail.com>
2015-06-12 19:00:40 -0300

    Added delete stash option and fix to prevent the stash when no files are open

--- tabstasher.py
+++ tabstasher.py
@@ -12,7 +12,10 @@ class TabstashCommand(sublime_plugin.WindowCommand):
         "name": '',
         "files" : filename_list
       }
-      self.window.show_input_panel("Stash Name:","", self.save_stash_name, None, None)
+      if len(filename_list) > 0:
+        self.window.show_input_panel("Stash Name:","", self.save_stash_name, None, None)
+      else:
+        sublime.message_dialog("No tabs to stash at the time")
 
     def save_stash_name(self, stash):
       default_settings = sublime.load_settings("Tabstasher.sublime-settings")
@@ -36,4 +39,18 @@ class TabunstashCommand(sublime_plugin.WindowCommand):
     def on_done(self, picked):
       for stashedFile in self.array_of_stashes[picked]['files']:
         self.window.open_file(stashedFile)
-      
+
+class TabdeletestashCommand(sublime_plugin.WindowCommand):
+
+    def run(self):
+      self.default_settings = sublime.load_settings("Tabstasher.sublime-settings")
+      if self.default_settings.has('stashes'):
+        self.array_of_stashes = self.default_settings.get('stashes')
+        array_of_stashes_names = []
+        for stash in self.array_of_stashes:
+          array_of_stashes_names.append(stash['name'])
+        self.window.show_quick_panel(array_of_stashes_names, self.on_done)
+        
+    def on_done(self, picked):
+      del self.array_of_stashes[picked]
+      self.default_settings.set('stashes',self.array_of_stashes)
